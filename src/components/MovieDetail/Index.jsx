47766a67d0908cda2bf2086582095785a1cf1ee5
Amit <amitkumar.developer1@gmail.com>
2021-01-09 19:39:52 +0530

    Movie page update

--- src/components/MovieDetail/Index.jsx
+++ src/components/MovieDetail/Index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import TopContainer from './TopContainer'
 import MainDetail from './MainDetail'
 import Review from './Review'
 import SimilarMovie from './SimilarMovie'
@@ -20,22 +19,28 @@ class Index extends React.Component{
     }
     render(){
         console.log(this.state.data)
-        return(
-            <div>
-                <TopContainer image={this.state.data.backdrop_path}/>
-                <MainDetail/>
-                <Review/>
-                <SimilarMovie/>
-                <RecommendedMovie/>
-                {this.state.data && this.state.data.images.backdrops.map((image,idx) => {
-                    return <img src={`https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${image.file_path}`} key={idx} alt="back" style={{width: '25%'}}/>
-                })}
-                <h1>Poster</h1>
-                {this.state.data && this.state.data.images.posters.map((image,idx) => {
-                    return <img src={`https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${image.file_path}`} key={idx} alt="poster" style={{width: '25%'}}/>
-                })}
-            </div>
-        )
+        if(this.state.data){
+
+            return(
+                <div>
+                    <MainDetail 
+                        title={this.state.data.original_title} 
+                        tagline={this.state.data.tagline} 
+                        poster_path={this.state.data.poster_path}
+                        bg_path={this.state.data.backdrop_path}
+                        year={this.state.data.release_date.substr(0,4)}
+                        data={this.state.data}
+                        />
+                    <Review/>
+                    <SimilarMovie/>
+                    <RecommendedMovie/>
+                </div>
+            )
+        }else{
+            return(
+                <h1>loading</h1>
+            )
+        }
     }
 }
 
--- src/components/MovieDetail/MainDetail.jsx
+++ src/components/MovieDetail/MainDetail.jsx
@@ -1,7 +1,52 @@
-const MainDetail = () => {
+import '../../css/MainDetail.css'
+const MainDetail = (props) => {
     return(
-        <div>
-            MainDetail
+        <div className="movie-detail" style={{backgroundImage: `url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${props.bg_path})`}}>
+            <div className="overlay">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-4 movie-image">
+                            <img src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`}/>
+                        </div>
+                        <div className="col-md-8 movie-text-data">
+                            <h1>{props.title} ({props.year})</h1>
+                            <p>
+                                <span><i class="fa fa-circle" aria-hidden="true"></i>{props.data.release_date}</span>
+                                <span>
+                                    <i class="fa fa-circle" aria-hidden="true"></i>
+                                    {
+                                        props.data.genres.map((item, idx) => {
+                                            if(idx !== props.data.genres.length-1){
+                                                return(`${item['name']}, `)
+                                            }
+                                            else{
+                                                return(item['name'])
+                                            }
+                                        })
+                                    }
+                                </span>
+                            </p>
+                            <p>
+                                Language: &nbsp;
+                                {
+                                    props.data.spoken_languages.map((item, idx) => {
+                                        if(idx !== props.data.spoken_languages.length-1){
+                                            return(`${item['name']}, `)
+                                        }
+                                        else{
+                                            return(item['name'])
+                                        }
+                                    })
+                                }
+                            </p>
+                            <p>
+                                Run time: {`${parseInt(props.data.runtime/60)}h ${props.data.runtime%60}min`}
+                            </p>
+                            <h6>{props.tagline}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
