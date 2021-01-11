5abd254b92ee108ce4bcf98f7986034a547b294f
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-12 00:36:10 +0530

    Updated

--- src/components/MovieDetail/Cast/CastData.jsx
+++ src/components/MovieDetail/Cast/CastData.jsx
@@ -1,8 +1,12 @@
+import {Link} from 'react-router-dom'
 const ReviewData = (props) => {
+    console.log(props)
     return(
         <center>
-            <img src={`https://image.tmdb.org/t/p/w500${props.data.profile_path}`} alt="cast" width="100" height="100" className="rounded-circle"/>
-            <small>{props.data.name}</small>
+            <Link to={`/person/${props.data.id}`} className="cast-name">
+                <img src={`https://image.tmdb.org/t/p/w500${props.data.profile_path}`} alt="cast" width="100" height="100" className="rounded-circle"/>
+                <small>{props.data.name}</small>
+            </Link>
         </center>
     )
 }
--- src/components/PersonDetail/PersonData.jsx
+++ src/components/PersonDetail/PersonData.jsx
@@ -1,20 +1,25 @@
+import '../../css/PersonData.css'
 const PersonData = (props)=>{
     const renderData=(data)=>{
         if(data){
             return(
-                <div className="container d-flex flex-row justify-content-between">
-                    <img className="p-4" style={{width:"20vw",height:"60vh"}} src={`https://image.tmdb.org/t/p/w500${data.profile_path}`} alt="actor"/>
-                    <div className="d-flex flex-column">
-                        <h1>{data.name}</h1>
-                        <p>{data.biography}</p>
-                        <h2>PERSONAL INFO</h2>
-                        <div className="d-flex flex-row flex-wrap justify-content-between">
-                            <span><h5>BIRTHDAY</h5>{data.birthday}</span>
-                            <span><h5>GENDER</h5>{data.gender}</span>
-                            <span><h5>KNOWN FOR</h5>{data.known_for_department}</span>
-                            <span><h5>PLACE OF BIRTH</h5>{data.place_of_birth}</span>
-                            <span><h5>POPULARITY</h5>{data.popularity}</span>
-                            <span><h5>ALSO KNOWN AS</h5>{data.also_known_as}</span>
+                <div className="container-fluid person-data">
+                    <div className="row">
+                        <div className="card-image col-md-3">
+                            <img src={`https://image.tmdb.org/t/p/w500${data.profile_path}`} alt="actor"/>
+                        </div>
+                        <div className="col-md-9">
+                            <h1>{data.name}</h1>
+                            <p>{data.biography}</p>
+                            <h2>PERSONAL INFO</h2>
+                            <div className="d-flex flex-row flex-wrap justify-content-between">
+                                <span><h5>BIRTHDAY</h5>{data.birthday}</span>
+                                <span><h5>GENDER</h5>{data.gender}</span>
+                                <span><h5>KNOWN FOR</h5>{data.known_for_department}</span>
+                                <span><h5>PLACE OF BIRTH</h5>{data.place_of_birth}</span>
+                                <span><h5>POPULARITY</h5>{data.popularity}</span>
+                                <span><h5>ALSO KNOWN AS</h5>{data.also_known_as}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
