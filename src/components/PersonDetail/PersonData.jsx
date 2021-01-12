566da7e09cb5555c131df81dc07623b91b0fce21
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-12 16:23:55 +0530

    Added responsive style

--- src/components/PersonDetail/PersonData.jsx
+++ src/components/PersonDetail/PersonData.jsx
@@ -5,11 +5,11 @@ const PersonData = (props)=>{
             return(
                 <div className="container-fluid person-data">
                     <div className="row">
-                        <div className="card-image col-md-3">
+                        <div className="card-image col-md-4 col-12 vh_center">
                             <img src={`https://image.tmdb.org/t/p/w500${data.profile_path}`} alt="actor"/>
                         </div>
-                        <div className="col-md-1"></div>
-                        <div className="col-md-8 person-text">
+                        {/* <div className="col-md-1"></div> */}
+                        <div className="col-md-8 person-text col-12 vh_center">
                             <h1>{data.name}</h1>
                             <div className="d-flex flex-row flex-wrap align-items-center">
                                 <h6 className="mr-2 text-warning">{data.birthday}</h6>
@@ -17,6 +17,7 @@ const PersonData = (props)=>{
                                 <h6 className="mr-2 text-warning">{data.known_for_department}</h6>
                                 <h6 className="mr-2 text-warning">{data.popularity}%</h6>
                             </div>
+                            <b>BIOGRAPHY: </b>
                             <p>{data.biography}</p>
                             {(data.also_known_as && data.also_known_as.length>1) &&
                                 <>
@@ -33,10 +34,20 @@ const PersonData = (props)=>{
                             }
                             <div>
                                 {data.imdb_id && 
-                                    <a rel="noopener noreferrer" target="_blank" href={`https://www.imdb.com/name/${data.imdb_id}`} className="btn btn-dark ml-2 mr-2 web-btn">IMDB <i className="fa fa-imdb" aria-hidden="true"></i> </a>
+                                    <a rel="noopener noreferrer" 
+                                        target="_blank" 
+                                        href={`https://www.imdb.com/name/${data.imdb_id}`} 
+                                        className="btn btn-dark ml-2 mr-2 web-btn">IMDB 
+                                        <i className="fa fa-imdb" aria-hidden="true"></i> 
+                                    </a>
                                 }
                                 {data.homepage && 
-                                    <a rel="noopener noreferrer" target="_blank" href={data.homepage} className="btn btn-dark ml-2 mr-2 web-btn">Website <i className="fa fa-link" aria-hidden="true"></i></a>
+                                    <a rel="noopener noreferrer" 
+                                        target="_blank" 
+                                        href={data.homepage} 
+                                        className="btn btn-dark ml-2 mr-2 web-btn">Website 
+                                        <i className="fa fa-link" aria-hidden="true"></i>
+                                    </a>
                                 }
                             </div>
                         </div>
