fa206d8d3f2d7c87f0aee8fc564afb777f9d2c5f
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-14 03:14:26 +0530

    Added loading spinner

--- src/components/MovieDetail/Index.jsx
+++ src/components/MovieDetail/Index.jsx
@@ -57,7 +57,11 @@ class Index extends React.Component{
             )
         }else{
             return(
-                <h1>loading</h1>
+                <div className="full-cover">
+                    <div className="spinner-border" style={{width: '3rem', height: '3rem'}} role="status">
+                    <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
             )
         }
     }
