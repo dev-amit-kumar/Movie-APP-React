377bf507d0535886013a549bc7b27ce658822c6a
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-14 14:38:46 +0530

    issue in review mobile view

--- src/components/MovieDetail/Review/ReviewData.jsx
+++ src/components/MovieDetail/Review/ReviewData.jsx
@@ -17,19 +17,21 @@ const ReviewData = (props) => {
                 <img src={img_src()} alt="review_person" width="50" height="50" className="rounded-circle mr-3"/>
                 <div className="mr-3">
                     <h6 className="text-uppercase">{props.data.author}</h6>
-                    <small>{props.data.created_at.slice(0,10)}</small>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <span className="mt-1">{props.data.created_at.slice(0,10)}</span>
+                        <ReactStars
+                            count={5}
+                            value={props.data.author_details.rating/2}
+                            edit={false}
+                            size={20}
+                            isHalf={true}
+                            emptyIcon={<i className="fa fa-star-o"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
+                    </div>
                 </div>
-                <ReactStars
-                    count={10}
-                    value={props.data.author_details.rating}
-                    edit={false}
-                    size={24}
-                    isHalf={true}
-                    emptyIcon={<i className="fa fa-star-o"></i>}
-                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                    fullIcon={<i className="fa fa-star"></i>}
-                    activeColor="#ffd700"
-                />
             </div>
             <span style={{fontSize: '20px'}}><i className="fa fa-quote-left" aria-hidden="true"></i></span>
             <div className="d-flex justify-content-center align-items-center">
