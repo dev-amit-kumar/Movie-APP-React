6c6b7619dcb17407ac66d4d327e71473f216df71
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-01-15 22:43:42 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,41 @@
-import '../css/Footer.css'
-
+import "../css/Footer.css"
 const Footer = () => {
     return (
         <footer>
-            <span className="text-white">&copy; DeveloperKumar Inc. All rights reserved 2020</span> 
-            <span className="text-white">Developed by: Amit Kumar</span>
+            <div class="row justify-content-center py-5 Footer">
+                <div class="col-11">
+                    <div class="row ">
+                        <div class="col-xl-8 col-md-4 col-sm-4 col-12  mx-auto a">
+                            <h3 class="text-muted mb-md-0 mb-5 bold-text">MovieTime.</h3>
+                        </div>
+                        <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                            <h6 class="mb-3 mb-lg-4 bold-text text-muted "><b>MENU</b></h6>
+                            <ul class="list-unstyled">
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Movie</li>
+                                <li>Actor</li>
+                            </ul>
+                        </div>
+                        <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                            <h6 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5"><b>Description</b></h6>
+                            <p class="mb-1">List of all movies</p>
+                            <p>And actors so that u can choose what to watch</p>
+                        </div>
+                    </div>
+                    <div class="row ">
+                        <div class="col-xl-8 col-md-4 col-sm-4 col-auto mt-5 order-sm-1 order-3 align-self-end">
+                            <p class="social text-muted mb-0 pb-0 bold-text"> <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></span> </p><small class="rights"><span>&#174;</span> MovieTime All Rights Reserved.</small>
+                        </div>
+                        <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
+                            <h6 class="mt-55 mt-2 text-muted bold-text"><b>Mukul Prasad</b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> Mukul@gmail.com</small>
+                        </div>
+                        <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3">
+                            <h6 class="text-muted bold-text"><b>Amit Kumar Tomar</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> amit@gmail.com</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
