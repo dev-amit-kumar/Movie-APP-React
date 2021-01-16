8c5bd188d4630e018c129597fb75a366333808cb
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-01-16 17:48:08 +0530

    login done

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,50 +1,38 @@
 import { Link } from "react-router-dom"
+import "../../css/Signup.css"
 
 const SignupForm=()=>{
     return(
-        <div className="card setting_common_card">
-            <h2 className="card-header">SignUp Form</h2>
-            <div className="card-body">
-                <form className="row">
-                    <div className="col-12">
-                        <h4>SignUp</h4>
-                    </div>
-                    <div className="form-group col-6">
-                        <label>First Name</label>
-                        <input className="form-control" name="fname"  type="text"/>
-                    </div><div className="form-group col-6">
-                        <label>Last Name</label>
-                        <input className="form-control" name="lname"  type="text"/>
-                    </div>
-                    <div className="form-group col-12">
-                        <label>Email id</label>
-                        <input className="form-control" name="emailid"  type="email"/>
-                    </div>
-                    <div className="form-group col-12">
-                        <label>Password</label>
-                        <input className="form-control" name="fname" type="password"/>
-                    </div>
-                    <div className="form-group col-6">
-                        <label>Phone Number</label>
-                        <input className="form-control" name="number"  type="number"/>
-                    </div>
-                    <div className="form-group col-6">
-                    <label>Gender</label>
-                        <div>
-                            <select className="btn btn-light">
-                                <option>Male</option>
-                                <option>Female</option>
-                                <option>Others</option>
-                            </select>
+        <div className="signup-outer-container">
+            <div className="signupConatiner">
+                <div className="signup-form">
+                    <div className="signup-inner card">
+                        <div className="card-header">
+                            <h2>SIGNUP</h2>
+                        </div>
+                        <div className="card-body">
+                        <form>
+                        <div class="form-group">
+                                <label for="exampleInputName1">Full Name</label>
+                                <input type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1"/>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                         </div>
                     </div>
-                    <div className="form-group col-12">
-                        <button className="btn btn-danger">SignUp</button>
-                    </div>
-                    <div className="form-group col-6">
-                        <Link>Already Have Account?</Link>
-                    </div>
-                </form>
+                </div>
+                <div className="container signin-form ">
+                    <h2>Already have an account?</h2>
+                    <Link className="btn btn-danger" to="/auth/login">Login</Link>
+                </div>
             </div>
         </div>
     )
