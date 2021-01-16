914c67ca1d1f074e87ab244c528add6849cf6646
mukul-prasad-au9 <mukulp0783@gmail.com>
2021-01-16 13:51:06 +0530

    login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,32 +1,18 @@
 import { Link } from "react-router-dom"
+import "../../css/Login.css"
 
 const LoginForm=()=>{
     return(
-        <div className="card setting_common_card">
-            <h2 className="card-header">Login Form</h2>
-            <div className="card-body">
-                <form className="row">
-                    <div className="col-12">
-                        <h4>Login</h4>
-                    </div>
-                    <div className="form-group col-6">
-                        <label>Email id</label>
-                        <input className="form-control" name="emailid"  type="email"/>
-                    </div>
-                    <div className="form-group col-6">
-                        <label>Password</label>
-                        <input className="form-control" name="fname" type="password"/>
-                    </div>
-                    <div className="form-group col-12">
-                        <Link>Forget Password?</Link>
-                    </div>
-                    <div className="form-group col-6">
-                        <button className="btn btn-success">Login</button>
-                    </div>
-                    <div className="form-group col-6">
-                        <Link>Don't Have Account?</Link>
-                    </div>
-                </form>
+        <div className="container mainConatiner">
+            <div className="login-form" >
+				<h2>LOGIN</h2>
+				<input className="form-control" type="text" placeholder="Email"/>
+				<input className="form-control" type="password" placeholder="Password"/>
+				<button className="btn btn-primary">Login</button>
+			</div>
+            <div className="container register-form ">
+                <h2>Don't have an account?</h2>
+                <button className="btn btn-danger">Register</button>
             </div>
         </div>
     )
