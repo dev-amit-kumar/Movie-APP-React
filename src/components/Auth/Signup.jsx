da8c1cf123b123d6c798270faf275929e2937248
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-17 14:04:11 +0530

    Updated login page

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -12,19 +12,19 @@ const SignupForm=()=>{
                         </div>
                         <div className="card-body">
                         <form>
-                        <div class="form-group">
-                                <label for="exampleInputName1">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp"/>
+                        <div className="form-group">
+                                <label htmlFor="exampleInputName1">Full Name</label>
+                                <input type="text" className="form-control" id="exampleInputName1" aria-describedby="nameHelp"/>
                             </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                             </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"/>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1"/>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
                         </div>
                     </div>
