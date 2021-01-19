77b0ee5fdb6916b2bc2486c4888341a8b9c76c2a
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-20 01:38:29 +0530

    Updated some code

--- src/redux/Firebase.js
+++ src/redux/Firebase.js
@@ -3,14 +3,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyD0-CXIGP7UxVQ25PukIoEg2b7CW2sHIN4",
-//     authDomain: "movie-time-3f213.firebaseapp.com",
-//     projectId: "movie-time-3f213",
-//     storageBucket: "movie-time-3f213.appspot.com",
-//     messagingSenderId: "992097171686",
-//     appId: "1:992097171686:web:f33c1ae660ed37b8d92327"
-// };
+var firebaseConfig = {
+	apiKey: 'AIzaSyD0-CXIGP7UxVQ25PukIoEg2b7CW2sHIN4',
+	authDomain: 'movie-time-3f213.firebaseapp.com',
+	projectId: 'movie-time-3f213',
+	storageBucket: 'movie-time-3f213.appspot.com',
+	messagingSenderId: '992097171686',
+	appId: '1:992097171686:web:f33c1ae660ed37b8d92327',
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 
