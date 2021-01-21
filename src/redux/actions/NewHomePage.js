2ea9e283b9533b4f3b52ec5ba80c09cfcae684c0
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-21 18:33:46 +0530

    updated packages

--- src/redux/actions/NewHomePage.js
+++ src/redux/actions/NewHomePage.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { apiKey } from '../config';
 const api_key = `api_key=${apiKey}`;
-// const url = 'https://api.themoviedb.org/3/movie';
 const turl = 'https://api.themoviedb.org/3/trending/movie';
 
 export const fetchTrendingMovieList = (type) => async (dispatch) => {
@@ -29,23 +28,7 @@ export const fetchTrendingMovieList = (type) => async (dispatch) => {
 		dispatch({ type: 'TOGGLE_IS_LOADING_TRENDING_MOVIE' });
 	}
 };
-// export const fetchRecommendedMovieList = () => async (dispatch) => {
-// 	try {
-// 		dispatch({ type: 'GET_RECOMMENDED_MOVIE', payload: null });
-// 		dispatch({ type: 'TOGGLE_IS_LOADING_RECOMMENDED_MOVIE' });
 
-// 		const { data } = await axios.get(
-// 			`${turl}/trending/movie/day?${api_key}&language=en-US&page=1`,
-//         );
-//         //api.themoviedb.org/3/trending/movie/day?api_key=911c65436dd290d171fc662603dac6b3
-// 		https: dispatch({
-// 			type: 'GET_RECOMMENDED_MOVIE',
-// 			payload: data.results,
-// 		});
-// 	} finally {
-// 		dispatch({ type: 'TOGGLE_IS_LOADING_RECOMMENDED_MOVIE' });
-// 	}
-// };
 export const fetchNowPlayingMovieList = () => async (dispatch) => {
 	try {
 		dispatch({ type: 'GET_NOW_PLAYING_MOVIE', payload: null });
